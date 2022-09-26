e3505b7f44149f4eb6ac0017ebaac47bce2f75ea
muhrusdi <mhmrofficial@gmail.com>
2022-09-27 00:27:27 +0700

    learn: checkpoint

--- src/stacks/array-based-stack-class.ts
+++ src/stacks/array-based-stack-class.ts
@@ -7,4 +7,20 @@ export class StackArray {
   push(element) {
     this.items.push(element)
   }
+
+  peek() {
+    return this.items[this.items.length - 1]
+  }
+
+  pop() {
+    this.items.pop()
+  }
+
+  clear() {
+    this.items = []
+  }
+
+  isEmpty() {
+    return this.items.length === 0
+  }
 }
--- src/stacks/index.ts
+++ src/stacks/index.ts
@@ -8,5 +8,6 @@ export const run = () => {
   stack.push(1)
   stack.push(2)
   stack.push(3)
+  console.log("last item -----", stack.peek())
   console.log(stack.items)
 }
