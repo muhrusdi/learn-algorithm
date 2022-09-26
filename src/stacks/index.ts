4a00a14febf8eb102a0b01447dae4adfdea81ba6
Muhammad Rusdi <mhmrofficial@gmail.com>
2022-09-26 16:57:17 +0000

    learn: add stack

--- src/stacks/index.ts
+++ src/stacks/index.ts
@@ -1,3 +1,19 @@
+class Stack {
+  items = []
+  constructor() {
+    this.items = []
+  }
+
+  push(element) {
+    this.items.push(element)
+  }
+}
+
 export const run = () => {
-  console.log("test")
+
+  const stack = new Stack()
+  stack.push(1)
+  stack.push(2)
+  stack.push(3)
+  console.log(stack.items)
 }
