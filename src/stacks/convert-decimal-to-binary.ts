ad60e28e84a33098e7ad95201e4eed566422f2cf
muhrusdi <mhmrofficial@gmail.com>
2022-09-28 21:19:40 +0700

    learn: add baseConverter

--- src/stacks/convert-decimal-to-binary.ts
+++ src/stacks/convert-decimal-to-binary.ts
@@ -2,6 +2,7 @@ import { StackArray } from "./array-based-stack-class"
 
 export const convertDecimealtoBinary = (decNumber) => {
   const remStack = new StackArray()
+  
   let number = decNumber
   let rem
   let binaryString = ""
@@ -14,6 +15,31 @@ export const convertDecimealtoBinary = (decNumber) => {
 
   while (!remStack.isEmpty()) {
     binaryString += remStack.pop().toString()
+  }
+
+
+  return binaryString
+}
+
+export const baseConverter = (decNumber, base) => {
+  const remStack = new StackArray()
+  const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let number = decNumber
+  let rem
+  let binaryString = ""
+
+  if (!(base >= 2 && base <= 36)) {
+    return ""
+  }
+
+  while (number > 0) {
+    rem = Math.floor(number % base)
+    remStack.push(rem)
+    number = Math.floor(number / base)
+  }
+
+  while (!remStack.isEmpty()) {
+    binaryString += digits[remStack.pop()]
   }
 
 
--- src/stacks/index.ts
+++ src/stacks/index.ts
@@ -1,6 +1,6 @@
 import { StackObj } from "./object-based-stack-class"
 import { StackArray } from "./array-based-stack-class"
-import { convertDecimealtoBinary } from "./convert-decimal-to-binary"
+import { convertDecimealtoBinary, baseConverter } from "./convert-decimal-to-binary"
 
 export const run = () => {
 
@@ -13,5 +13,9 @@ export const run = () => {
   console.log("last item -----", stack.pop())
   console.log("peek -----", stack.peek())
   console.log("binary -----", convertDecimealtoBinary(40))
+  console.log("binary -----", baseConverter(40, 30))
+  console.log("binary2 -----", baseConverter(4034, 12))
+  console.log("binary3 -----", baseConverter(40444, 23))
+  console.log("binary4 -----", baseConverter(40565757, 3))
   console.log(stack._items)
 }
